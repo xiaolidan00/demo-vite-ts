fbd8d7c8a5524ab00ad6e86d993b31f1c28607f2
xiaolidan00 <764937567@qq.com>
2025-05-27 19:46:33 +0800

    fix: webcomponent 表单

--- src/webcomponent/index.ts
+++ src/webcomponent/index.ts
@@ -1,136 +1,74 @@
-class CustomInput extends HTMLElement {
+class CustomSwitch extends HTMLElement {
+  internals: ElementInternals;
   //开启关联表单元素
   static formAssociated = true;
-  internals: ElementInternals;
-  input: HTMLInputElement;
-  num: HTMLSpanElement;
-  wrap: HTMLDivElement;
   constructor() {
     super();
-    this.attachShadow({ mode: 'open', delegatesFocus: true });
-    const shadow = this.shadowRoot!;
-    //返回一个ElementInternals对象，允许自定义元素完全参与HTML表单
     this.internals = this.attachInternals();
+    // 创建影子节点
+    this.attachShadow({ mode: 'open', delegatesFocus: true });
+    //影子根节点
+    const shadow = this.shadowRoot!;
 
     const sheet = new CSSStyleSheet();
-    sheet.replaceSync(/*css*/ `*{
-  box-sizing:border-box;
+    sheet.replaceSync(/*css*/ ` *{
+        box-sizing:border-box;
+      }
+  :host {
+    display: inline-flex;
+    height:20px;
+    width:40px;
+    background:white;   
+    border-radius:10px;
+    overflow:hidden;      
+     padding:2px;
+     background:gray;
   }
-  :host{
-  display:inline-block;
+  :host::before {
+    display:flex;
+    align-items:center;
+    justify-content:flex-start;
+    height:16px;
+    width:16px;
+    border-radius:50%;
+    content: "";  
+    background:white;
   }
-  .input-wrapper{
-  display:inline-flex; 
-  border-radius:4px;
-  height:32px;
-  border:solid 1px gray;
-  align-items:center;
-  padding:0 10px;
-  }
-   .input-wrapper.error{
-     border:solid 1px red;
-   }
-   .input-wrapper.error .num{
-    color:red;
-  }
-  input{
-  border:none;
-  background:transparent;  
-  height:30px;
-  outline:none;
-  display:inline-block;  
-  }
-  .num{
-  display:inline-block;
-  font-size:12px;
-  color:gray; 
-  }  `);
+  :host(:state(checked)){
+  background:dodgerblue;
+  justify-content:flex-end;
+  transition:all 0.5s ease;
+  } `);
     shadow.adoptedStyleSheets = [sheet];
-    shadow.innerHTML = /*html*/ `<div class="input-wrapper"> 
-  <input type='text' placeholder="${this.getAttribute('placeholder') || ''}" /> 
-  <span class='num'></span>
-</div>`;
-    this.wrap = shadow.querySelector('.input-wrapper') as HTMLDivElement;
-    this.input = shadow.querySelector('input') as HTMLInputElement;
-    this.num = shadow.querySelector('.num') as HTMLSpanElement;
-    //设置输入值
-    this.setInputVal(this.getAttribute('value') || '');
-    //输入事件监听
-    this.input.oninput = () => {
-      const v = this.input.value;
-      //文本长度
-      this.updateNum();
-      //设置表单值
-      this.internals.setFormValue(v);
-      //表单验证
-      this.validate();
-    };
+    this.addEventListener('click', this.onClick.bind(this));
   }
-  //设置输入值
-  setInputVal(v: string) {
-    if (this.input) this.input.value = v;
-    this.updateNum();
-    this.internals.setFormValue(v);
-    this.validate();
-  }
-  set value(v: string) {
-    this.setInputVal(v);
-  }
-  get value() {
-    return this.input?.value || '';
-  }
-  connectedCallback() {
-    //获取关联表单
-    console.log(this.internals.form);
+  onClick() {
+    this.checked = !this.checked;
   }
   disconnectedCallback() {
-    this.input.oninput = null;
+    this.removeEventListener('click', this.onClick.bind(this));
   }
-  //表单自带验证
-  validate() {
-    if (this.getAttribute('required') === 'true' && this.input.value === '') {
-      this.internals.setValidity({ valueMissing: true }, '请输入' + (this.getAttribute('label') || ''));
-    } else {
-      this.internals.setValidity({});
-    }
-
-    this.internals.reportValidity();
+  get checked() {
+    return this.internals.states.has('checked');
   }
 
-  updateNum() {
-    const v = this.input.value;
-    const len = Number(this.getAttribute('maxlength')) || 0;
-    this.num.innerHTML = `${v.length}/${len}`;
-    //超过长度则显示错误样式
-    if (v.length <= len) {
-      this.wrap.classList.remove('error');
+  set checked(flag) {
+    //设置状态值
+    if (flag) {
+      this.internals.states.add('checked');
+      this.internals.setFormValue('checked', 'checked');
     } else {
-      this.wrap.classList.add('error');
+      this.internals.states.delete('checked');
+      this.internals.setFormValue('checked', '');
     }
   }
-  static observedAttributes = ['maxlength', 'placeholder', 'required'];
-  attributeChangedCallback(name: string, oldValue: string, newValue: string) {
-    switch (name) {
-      case 'placeholder':
-        this.input.placeholder = newValue;
-        break;
-      case 'maxlength':
-        //文本长度
-        this.updateNum();
-        break;
-      case 'required':
-        //表单验证
-        this.validate();
-        break;
-    }
+
+  //判断状态语法是否可用
+  static isStateSyntaxSupported() {
+    return CSS.supports('selector(:state(checked))');
   }
 }
-customElements.define('custom-input', CustomInput);
-{
-  const cinput = new CustomInput();
-  cinput.setAttribute('placeholder', '请输入数值');
-  cinput.setAttribute('required', 'true');
-  cinput.setAttribute('maxlength', '10');
-  cinput.value = '1234';
-  document.body.appendChild(cinput);
-}
+
+customElements.define('custom-switch', CustomSwitch);
+const switchEl = new CustomSwitch();
+document.body.appendChild(switchEl);
--- src/webcomponent/input.ts
+++ src/webcomponent/input.ts
@@ -4,6 +4,8 @@ class CustomInput extends HTMLElement {
   internals: ElementInternals;
   input: HTMLInputElement;
   num: HTMLSpanElement;
+  tip: HTMLDivElement;
+  wrap: HTMLDivElement;
   constructor() {
     super();
     this.attachShadow({ mode: 'open', delegatesFocus: true });
@@ -11,9 +13,8 @@ class CustomInput extends HTMLElement {
     //返回一个ElementInternals对象，允许自定义元素完全参与HTML表单
     this.internals = this.attachInternals();
 
-    shadow.innerHTML = /*html*/ `
-  <style>
-  *{
+    const sheet = new CSSStyleSheet();
+    sheet.replaceSync(/*css*/ `*{
   box-sizing:border-box;
   }
   :host{
@@ -25,11 +26,17 @@ class CustomInput extends HTMLElement {
   height:32px;
   border:solid 1px gray;
   align-items:center;
+  padding:0 10px;
+  }
+   .input-wrapper.error{
+     border:solid 1px red;
+   }
+   .input-wrapper.error .num{
+    color:red;
   }
   input{
   border:none;
-  background:transparent;
-  padding:0 10px;
+  background:transparent;  
   height:30px;
   outline:none;
   display:inline-block;  
@@ -38,57 +45,37 @@ class CustomInput extends HTMLElement {
   display:inline-block;
   font-size:12px;
   color:gray; 
-  } 
-  .num.error{
-    color:red;
   }
-  </style>
-  <div class="input-wrapper"> 
-  <input type='text' maxlength="${this.getAttribute('maxlength') || ''}"  placeholder="${
-      this.getAttribute('placeholder') || ''
-    }" /> 
+  .error-tip{
+    padding:5px;
+    font-size:12px;
+    color:red;
+  }  `);
+    shadow.adoptedStyleSheets = [sheet];
+    shadow.innerHTML = /*html*/ `<div class="input-wrapper"> 
+  <input type='text' placeholder="${this.getAttribute('placeholder') || ''}" /> 
   <span class='num'></span>
-</div>`;
-
+</div>
+<div class="error-tip"></div>`;
+    this.wrap = shadow.querySelector('.input-wrapper') as HTMLDivElement;
     this.input = shadow.querySelector('input') as HTMLInputElement;
     this.num = shadow.querySelector('.num') as HTMLSpanElement;
+    this.tip = shadow.querySelector('.error-tip') as HTMLDivElement;
+    //设置输入值
     this.setInputVal(this.getAttribute('value') || '');
+    //输入事件监听
     this.input.oninput = () => {
+      console.log(this.internals.form);
       const v = this.input.value;
+      //文本长度
       this.updateNum();
+      //设置表单值
       this.internals.setFormValue(v);
+      //表单验证
       this.validate();
     };
   }
-  connectedCallback() {
-    //获取关联表单
-    console.log(this.internals.form);
-  }
-  validate() {
-    if (this.input.value === '') {
-      this.internals.setValidity({ valueMissing: true }, '请输入' + (this.getAttribute('label') || ''));
-    } else {
-      this.internals.setValidity({});
-    }
-
-    this.internals.reportValidity();
-  }
-  get value() {
-    return this.input?.value || '';
-  }
-  updateNum() {
-    if (this.num) {
-      const v = this.input.value;
-      const len = Number(this.getAttribute('maxLength')) || 0;
-      this.num.innerHTML = `${v.length}/${len}`;
-      if (v.length <= len) {
-        this.num.classList.remove('error');
-      } else {
-        this.num.classList.add('error');
-      }
-    }
-  }
-
+  //设置输入值
   setInputVal(v: string) {
     if (this.input) this.input.value = v;
     this.updateNum();
@@ -98,30 +85,72 @@ class CustomInput extends HTMLElement {
   set value(v: string) {
     this.setInputVal(v);
   }
+  get value() {
+    return this.input?.value || '';
+  }
+  connectedCallback() {
+    //获取关联表单
+    console.log(this.internals.form);
+  }
+  disconnectedCallback() {
+    this.input.oninput = null;
+  }
+  //表单自带验证
+  validate() {
+    if (this.input.value.length > Number(this.getAttribute('maxlength'))) {
+      const text = '最多输入10个字符';
+      this.tip.innerHTML = text;
+      this.tip.style.display = 'block';
+      this.internals.setValidity({ tooLong: true }, text, this.tip);
+    } else if (this.getAttribute('required') === 'true' && this.input.value === '') {
+      const text = this.getAttribute('placeholder') || '请输入';
+      this.tip.innerHTML = text;
+      this.tip.style.display = 'block';
+      this.internals.setValidity({ valueMissing: true }, text, this.tip);
+    } else {
+      this.internals.setValidity({});
+      this.tip.style.display = 'none';
+    }
+    this.internals.reportValidity();
+  }
 
-  static observedAttributes = ['maxLength', 'placeholder', 'required'];
+  updateNum() {
+    const v = this.input.value;
+    const len = Number(this.getAttribute('maxlength')) || 0;
+    this.num.innerHTML = `${v.length}/${len}`;
+    //超过长度则显示错误样式
+    if (v.length <= len) {
+      this.wrap.classList.remove('error');
+    } else {
+      this.wrap.classList.add('error');
+    }
+  }
+  static observedAttributes = ['maxlength', 'placeholder', 'required', 'name'];
   attributeChangedCallback(name: string, oldValue: string, newValue: string) {
     switch (name) {
-      case 'required':
-        this.input.required = newValue === 'true';
-        break;
       case 'placeholder':
         this.input.placeholder = newValue;
         break;
-      case 'maxLength':
-        const v = Number(newValue);
-        if (v > 0) this.input.maxLength = v;
+      case 'maxlength':
+        //文本长度
         this.updateNum();
+        break;
+      case 'required':
+        //表单验证
+        this.validate();
         break;
     }
   }
 }
 customElements.define('custom-input', CustomInput);
-{
-  const cinput = new CustomInput();
-  cinput.setAttribute('placeholder', '请输入数值');
-  cinput.setAttribute('required', 'true');
-  cinput.setAttribute('maxLength', '10');
-  cinput.value = '1234';
-  document.body.appendChild(cinput);
-}
+
+const cinput = new CustomInput();
+cinput.setAttribute('placeholder', '请输入数值');
+cinput.setAttribute('required', 'true');
+cinput.setAttribute('maxlength', '10');
+cinput.value = '1234';
+document.body.appendChild(cinput);
+
+const form = document.createElement('form');
+form.appendChild(cinput);
+document.body.appendChild(form);
